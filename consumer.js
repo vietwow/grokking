fc734e108c762534be6ea3dc771f52c3d86d305c
Viet Nguyen Chan <vietwow@Viets-MacBook-Pro-2.local>
2016-11-21 15:27:17 +0700

    version 2 - get geo info from redis

--- consumer.js
+++ consumer.js
@@ -1,36 +1,52 @@
+var redis = require('redis').createClient();
+var geoip = require('redis-geo')(redis);
+
 var kafka = require('kafka-node');
 
-//pg config
-var pg = require('pg');
-var conString = 'postgres://postgres:newhacker@61.28.227.196/grok';
+//es config
+var elasticsearch = require('elasticsearch')
 
-var kafkaClient = new kafka.Client('10.60.3.132:2181', 'test-consumer');
+// Connect to localhost:9200 and use the default settings
+var client = new elasticsearch.Client();
 
+// Connect the client to two nodes, requests will be
+// load-balanced between them using round-robin
+var client = elasticsearch.Client({
+  hosts: [
+    'http://61.28.227.202:9200'
+  ]
+});
+
+
+var kafkaClient = new kafka.Client('zoo1:2181', 'test-consumer');
+
+/*
 var topics = [
-     { topic: 'logs', partition: 0 }
+     { topic: 'events', partition: 0 }
 ];
+*/
 
-var kafkaConsumer = new kafka.Consumer(kafkaClient, topics, {
+var kafkaConsumer = new kafka.Consumer(kafkaClient, [{topic: 'events'}], {
      autoCommit: false
 });
 
+
 kafkaConsumer.on('message', function (message) {
      //console.log('MESSAGE');
-     //console.log(message);
-
-     console.log(JSON.parse(message.value));
-     pg.connect(conString, function(err, client, done) {
-       if (err) {
-         return console.error('error fetching client from pool', err);
-       }
-       console.log("connected to database");
-       client.query('INSERT INTO logs.events_write(ts, body) VALUES(CURRENT_TIMESTAMP, $1)', [JSON.parse(message.value)], function(err, result) {
-         done(); //this done callback signals the pg driver that the connection can be closed or returned to the connection pool
-         if (err) {
-           return console.error('error running query', err);
-         }
-         console.log(result);
-       });
+     //console.log(message.value);
+     body = JSON.parse(message.value)
+     body.timestamp = new Date()
+     geoip(body.ip, function (err, info) {
+       if (err) throw err;
+	body.geoinfo = info
+        //console.log(info);
+        //console.log(body.geoinfo);
+	client.index({
+	       index: 'grok',
+	       type: body.metric,
+	       body: body,
+	       refresh: true
+	})
      });
 });
 
